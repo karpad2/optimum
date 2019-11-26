2ef052f6cfcbefd77ee05f5e40f59d2dca1b8ac1
karpad2@gmail.com <karpad2@gmail.com>
2019-11-26 21:53:11 +0100

    +függvények

--- szem.m
+++ szem.m
@@ -14,8 +14,7 @@ funs=myfuns();
 
 options_dfp = optimset('Display','iter','LargeScale','off','HessUpdate','dfp'); %DFP;
 options_bfgs = optimset('Display','iter','LargeScale','off'); %BFGS
-options_qnewton = optimoptions('fminunc','Algorithm','quasi-newton');
-options_grad = optimset('Display','iter','GradObj','on'); %Gradiensel való számolás
+options_qnewton = optimoptions('fminunc','Display','iter','Algorithm','quasi-newton');
 
 deltaf=[];deltafval=[];
 
@@ -24,11 +23,15 @@ f1=[];f2=[];f3=[];
 x=1:length(funs);
 for i=x
   x0=[0,0];
-  f1(i,:)=fminunc(funs{i},x0,options_qnewton);% quasi newton
+  disp(funs{i})
+  disp('Quasi Newton:')
+  [f1(i,:),iter]=fminunc(funs{i},x0,options_qnewton);
+  disp('BFGS:')
   f2(i,:)=fminunc(funs{i},x0,options_bfgs);% bfgs quasi newton
+  disp('Vonalmenti:')
   f3(i,:)=fminsearch(funs{i},x0);% vonalmenti
 end
 
 
 
-plot(x,f1,f2,f3)
+plot(x,f1(:,1)',f2(:,1)',f3(:,1)')
